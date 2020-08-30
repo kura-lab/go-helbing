2a21b0a3c89821badb54f7a1337a5afe292ef08d
kura <mkurahay@gmail.com>
2020-08-31 01:10:31 +0900

    added main function and initialized rand function.

--- helbing.go
+++ helbing.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	crand "crypto/rand"
+	"math"
+	"math/big"
+	"math/rand"
+
 	"github.com/faiface/pixel"
 )
+
+var pedestrians [1]*pedestrian
 
 type pedestrian struct {
 	pixel.Vec
@@ -15,4 +22,11 @@ func newPedestrian() *pedestrian {
 	p.X = 100
 	p.Y = 100
 	return p
+}
+
+func main() {
+	seed, _ := crand.Int(crand.Reader, big.NewInt(math.MaxInt64))
+	rand.Seed(seed.Int64())
+
+	pedestrians[0] = newPedestrian()
 }
