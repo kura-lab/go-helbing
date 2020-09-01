9d1ff8ecd22ea8e44075b48148135986d42bef4e
kura <mkurahay@gmail.com>
2020-09-01 23:45:03 +0900

    added a member of color in pedestrian struct.

--- helbing.go
+++ helbing.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	crand "crypto/rand"
+	"image/color"
 	"math"
 	"math/big"
 	"math/rand"
@@ -15,12 +16,14 @@ type pedestrian struct {
 	pixel.Vec
 	X float64
 	Y float64
+	C color.RGBA
 }
 
 func newPedestrian() *pedestrian {
 	p := new(pedestrian)
 	p.X = 100
 	p.Y = 100
+	p.C = color.RGBA{157, 180, 255, 255}
 	return p
 }
 
