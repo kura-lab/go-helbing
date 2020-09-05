0c8e9ac00ceba387d138bffc87173ebc854f77a5
kura <mkurahay@gmail.com>
2020-09-05 23:45:22 +0900

    changed to repeat predestrian's position.

--- helbing.go
+++ helbing.go
@@ -25,17 +25,24 @@ type pedestrian struct {
 
 func newPedestrian() *pedestrian {
 	p := new(pedestrian)
-	p.X = 100
-	p.Y = 100
+	p.X = random(0, width)
+	p.Y = random(0, height)
 	p.C = color.RGBA{157, 180, 255, 255}
 	return p
 }
 
 func (p *pedestrian) update() {
 
-	if p.X > 200 {
-		p.X = 100
-		p.Y = 100
+	if p.X > width {
+		p.X = 0
+	} else if p.X < 0 {
+		p.X = width
+	}
+
+	if p.Y > height {
+		p.Y = 0
+	} else if p.Y < 0 {
+		p.Y = height
 	}
 
 	p.X = p.X + 1
@@ -44,8 +51,8 @@ func (p *pedestrian) update() {
 
 func (p *pedestrian) draw(imd *imdraw.IMDraw) {
 	pix := pixel.V(
-		p.X,
-		p.Y,
+		width/2-p.X,
+		height/2-p.Y,
 	)
 
 	imd.Color = p.C
@@ -89,4 +96,8 @@ func main() {
 		}
 	},
 	)
+}
+
+func random(min, max float64) float64 {
+	return rand.Float64() * (max - min)
 }
